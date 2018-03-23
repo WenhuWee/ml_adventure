f339c5dcf6948ed8e700f38fda3d6c1a8442a490
ewenli <ewenli@tencent.com>
2018-03-23 16:47:15 +0800

    more distance

--- base/distance.py
+++ base/distance.py
@@ -1,5 +1,8 @@
 #!/usr/local/bin/python3
 
+import numpy as np
+# import scipy as sp
+
 
 def minkDistance(vector1, vector2, dimenstion=2):
     distance = 0.0
@@ -27,5 +30,43 @@ def chebyDistance(vector1, vector2):
     return distance
 
 
-x = chebyDistance([1, 2], [3, 5])
+def _mahaDistance(vector1, vector2, cov):
+    npVec1 = np.array([vector1])
+    npVec2 = np.array([vector2])
+
+    minus = npVec1 - npVec2
+
+    invCov = np.linalg.inv(cov)
+
+    distance = np.sqrt(np.dot(np.dot(minus, invCov), minus.T))
+    return distance
+
+
+def mahaDistance(vector1, vector2, vectors):
+    vecs = np.array(vectors)
+    vecs = vecs.transpose()
+    cov = np.cov(vecs)
+    return _mahaDistance(vector1, vector2, cov)
+
+
+def cosineDistance(vector1, vector2):
+    distance = 0.0
+
+    if len(vector1) == len(vector2):
+        x = np.dot(vector1, vector2)
+
+        lengthFunc = lambda vector:pow(sum(map(lambda vec1: pow(vec1, 2), vector)), 0.5)
+
+        y = lengthFunc(vector1) * lengthFunc(vector2)
+        print(x, y)
+        distance = x / y
+
+    return distance
+
+
+x = mahaDistance([3, 4], [5, 6], [[3, 4], [5, 6], [2, 2], [8, 4]])
+print(x)
+
+
+x = cosineDistance([1, 4], [3, 2])
 print(x)
