79acbff0402489d568c90e48c8c04be4338c2c63
wenhu <wenbulli3@gmail.com>
2018-04-03 09:33:31 +0800

    gd

--- linear_model/gradientDescent.py
+++ linear_model/gradientDescent.py
@@ -1,35 +1,37 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import linear_model
 
 
-def gradientDescent(x, y, lr=0.1, updateBatch=1):
+def gradientDescent(x, y, lr=0.01, updateBatch=1):
     npX = x
     npY = y
-    w = np.ones(npX.ndim)
+    w = np.ones(npX.shape[1])
     b = 0
 
-    sumDeltaW = np.zeros(2)
+    sumDeltaW = np.zeros(npX.shape[1])
     sumDeltaB = 0
 
     for index, x_ in enumerate(npX):
 
-        print()
-        print(index)
-        print(w)
-        print(b)
+        # print()
+        # print(index)
+        # print(w)
+        # print(b)
 
         y_ = npY[index]
 
-        # print(y_)
+        # print(x_,y_)
 
         sumDeltaB = np.dot(x_, w) - y_
         sumDeltaW = x_ * (np.dot(x_, w) + b - y_)
 
-        if (index + 1) % updateBatch == 0:
+        # print(sumDeltaW, sumDeltaB)
 
-            print(index)
+        if (index + 1) % updateBatch == 0:
 
             w -= lr * sumDeltaW / updateBatch
             sumDeltaW = np.zeros(2)
@@ -40,23 +42,35 @@ def gradientDescent(x, y, lr=0.1, updateBatch=1):
     return (w, b)
 
 
+count = 500
+des = 1
 np.random.seed(123)
-x = 100 * np.random.rand(5, 2)
-w = np.array([3, 2])
-y = 5 + np.dot(x, w) + np.random.randn(1) * 10
+
+x = 10 * np.random.rand(count, des)
+w = np.array([4])
+y = np.dot(x, w) + np.random.randn(count) * w[0].mean()/2
 
 (w_p, b_p) = gradientDescent(x, y)
 
-print(w_p)
-print(b_p)
+# clf = linear_model.SGDRegressor()
+# clf.fit(x, y)
 
-# x_test = 100 * np.random.rand(50, 2)
-# y_p = b_p + np.dot(x_test, w_p)
+print(w_p,b_p)
 
-# fig = plt.figure(figsize=(8, 6))
-# plt.scatter(x, y)
-# plt.scatter(x_test, y_p)
-# plt.title("Dataset")
-# plt.xlabel("x")
-# plt.ylabel("y")
+x_test = 10 * np.random.rand(50, des)
+y_p = b_p + np.dot(x_test, w_p)
+
+# y_p = clf.predict(x_test)
+
+#
+fig = plt.figure()
+plt.scatter(x, y)
+plt.scatter(x_test, y_p)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+# ax = Axes3D(fig)
+# ax.scatter(x[:,0], x[:,1], y)
+# ax.scatter(x_test[:,0], x_test[:,1], y_p)
 # plt.show()
