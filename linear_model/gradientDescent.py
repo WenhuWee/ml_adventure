3df3e731692632e5dd7799dc50cc85d379777bc9
ewenli <ewenli@tencent.com>
2018-04-09 20:54:04 +0800

    loss

--- linear_model/gradientDescent.py
+++ linear_model/gradientDescent.py
@@ -6,7 +6,10 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import linear_model
 
 
-def gradientDescent(x, y, lr=0.01, updateBatch=1):
+def gradientDescent(x, y, lr=0.01, updateBatch=2):
+
+    count = x.shape[0]
+
     npX = x
     npY = y
     w = np.ones(npX.shape[1])
@@ -14,27 +17,28 @@ def gradientDescent(x, y, lr=0.01, updateBatch=1):
 
     sumDeltaW = np.zeros(npX.shape[1])
     sumDeltaB = 0
+    sumloss = 0
+
+    cost = []
 
     for index, x_ in enumerate(npX):
 
-        # print()
-        # print(index)
-        # print(w)
-        # print(b)
-
         y_ = npY[index]
 
-        # print(x_,y_)
+        sumloss += (np.dot(x_, w) + b - y_)**2
 
-        sumDeltaB = np.dot(x_, w) - y_
-        sumDeltaW = x_ * (np.dot(x_, w) + b - y_)
+        sumDeltaB += np.dot(x_, w) + b - y_
+        sumDeltaW += x_ * (np.dot(x_, w) + b - y_)
 
-        # print(sumDeltaW, sumDeltaB)
+        if count > 100 and index % (count / 100) == 0:
+            averageLoss = sumloss / 100
+            print('loss:', averageLoss)
+            cost.append(averageLoss)
+            sumloss = 0
 
         if (index + 1) % updateBatch == 0:
-
             w -= lr * sumDeltaW / updateBatch
-            sumDeltaW = np.zeros(2)
+            sumDeltaW = np.zeros(npX.shape[1])
 
             b -= lr * sumDeltaB / updateBatch
             sumDeltaB = 0
@@ -42,26 +46,24 @@ def gradientDescent(x, y, lr=0.01, updateBatch=1):
     return (w, b)
 
 
-count = 500
+count = 5000
 des = 1
 np.random.seed(123)
 
 x = 10 * np.random.rand(count, des)
 w = np.array([4])
-y = np.dot(x, w) + np.random.randn(count) * w[0].mean()/2
+y = np.dot(x, w) + np.random.randn(count) * w[0].mean() / 2
 
 (w_p, b_p) = gradientDescent(x, y)
+print(w_p, b_p)
 
-# clf = linear_model.SGDRegressor()
+# clf = linear_model.SGDRegressor(penalty='none')
 # clf.fit(x, y)
-
-print(w_p,b_p)
 
 x_test = 10 * np.random.rand(50, des)
 y_p = b_p + np.dot(x_test, w_p)
-
-# y_p = clf.predict(x_test)
-
+#
+# # y_p = clf.predict(x_test)
 #
 fig = plt.figure()
 plt.scatter(x, y)
